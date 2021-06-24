b9913e92e971113db823577c9ba1aac5ce7286a0
luiz.zortea <luiz.zortea@el.com.br>
2021-06-24 20:08:31 -0300

    Algumas novas features

--- bot/app/Bot.py
+++ bot/app/Bot.py
@@ -1,6 +1,9 @@
 import discord
+from discord.ext import commands
 
 client = discord.Client()
+
+comando = commands.Bot(command_prefix = '>', case_insensitivite = True)
 
 @client.event
 async def on_ready():
@@ -15,14 +18,22 @@ async def on_message(msg):
         await msg.channel.send('Alo eu')
 
     if (msg.content.startswith('help')):
-        vc = discord.Member()
-        vc.move_to(None)
+        vc = msg.author
+        await vc.move_to(None)
 
 @client.event
 async def on_member_join(member):
     for channel in member.server.channels:
         if channel.name == 'testando-o-bot':
             await client.send_message(channel, 'Message to send when member joins')
+
+@comando.command()
+async def ola(ctx):
+    await ctx.send(f'Ola, {ctx.author}')
+
+@comando.command()
+async def dado(ctx, numero):
+    await ctx.send(numero)
 
 
 client.run('ODU3MzA1MjQ1MjI2MjM3OTUy.YNNpug._NdY6AEKnKW6qcfKAOrko6rJlUM')
