3646efca2c313341286782bf0b4416994e9fea66
luiz.zortea <luiz.zortea@el.com.br>
2021-06-24 22:28:15 -0300

    Alguns comentários e novo token

--- bot/app/Bot.py
+++ bot/app/Bot.py
@@ -14,9 +14,17 @@ async def on_message(msg):
     if msg.author == client.user:
         return
         
+    '''
+        Enviando uma mensagem no canal -> channel.send(msg)
+        Nesse caso, peguei o canal, pelo contexto da mensagem enviada
+    '''
     if (msg.content.startswith('Alo')):
         await msg.channel.send('Alo eu')
 
+    '''
+        Para remover um usuario do canal de voz -> user.move_to(None)
+        Nesse caso, peguei o user pelo autor da mensagem
+    '''
     if (msg.content.startswith('help')):
         vc = msg.author
         await vc.move_to(None)
@@ -36,5 +44,5 @@ async def dado(ctx, numero):
     await ctx.send(numero)
 
 
-client.run('ODU3MzA1MjQ1MjI2MjM3OTUy.YNNpug._NdY6AEKnKW6qcfKAOrko6rJlUM')
+client.run('ODU3MzA1MjQ1MjI2MjM3OTUy.YNNpug.XIGUhHcA6TUZCJuoDZfeYLuV26c')
 
